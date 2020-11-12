e5cdc0e0c6d133c095f88dec1a5a4bc320e45444
Dichill <48512644+Dichill@users.noreply.github.com>
2020-11-12 12:24:56 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,6 +28,10 @@
 # https://stackoverflow.com/questions/52905578/extracting-elements-without-class-or-id-using-beautifulsoup/52907043
 # https://stackoverflow.com/questions/36165854/beautifulsoup-scraping-information-from-multiple-divs-using-loops-into-json
 # https://stackoverflow.com/questions/62740766/how-to-select-specific-div-using-beautifulsoup-when-multiple-divs-have-the-same
+# https://stackoverflow.com/questions/57013133/how-to-extract-the-on-click-value/57013342
+# https://stackoverflow.com/questions/39289206/how-to-get-href-link-from-onclick-function-in-python
+# https://stackoverflow.com/questions/58443772/get-url-from-onclick-attribute-of-a-tag-python
+# https://stackoverflow.com/questions/55056003/beautiful-soup-get-data-inside-a-button-tag
 # and a lot more! Just do a handful of google search and there's a lot of topics that revolves around BS4
 
 # Documentation
@@ -35,20 +39,28 @@
 # https://tedboy.github.io/bs4_doc/
 # https://www.kite.com/python/docs/bs4.BeautifulSoup
 
+# Important!
+IsDebugging = True
+
 from bs4 import BeautifulSoup
+import json
 import requests
 
 # Urls
 url = 'https://smtickets.com/' # Main Domain of the SM Website
-searchurl = 'https://smtickets.com/events/search/' # Search URL
+searchurl = url + 'events/search/' # Search URL
+
+# Store the Error Message
+ErrorMessage = set()
+IsError = False
 
 # Example of what to search. This will be used on Django sooner or later.
-Search = "Demon Slayer"
-
+Search = input("Title of Movie: ")
 
 # This is where the magic begins
-def StartScrape():
+def FetchMovieResults():
     # We want to make Search global so that we have access to it.
+    global ErrorMessage
     global Search
 
     # We want to replace whitespace with %20 since. -------  "which indicates space in website."
@@ -58,7 +70,7 @@ def StartScrape():
     #              Duplicates that are going to be stored later on.
     
     movie_title = set()
-    movie_date = set()
+    movie_date = []
     movie_location = set()
     movie_link = set()
 
@@ -89,45 +101,80 @@ def StartScrape():
                 result = wrapper.find_all("ul", {"class":"c-list__items c-list__items--option-5"})
                 for x in result:
                     li = x.find("li")
-                    for l in li:
+                    
+                    #title = li.find('div') ~ NA
+                    #data.add(article.text) ~ NA
 
-                        #title = li.find('div') ~ NA
-                        #data.add(article.text) ~ NA
+                    # Title - Gets the title of the Movie
+                    article = li.find_all('div', {"class":""})
+                    movie_title.add(article[0].text)
 
-                        # Title - Gets the title of the Movie
-                        article = li.find_all('div', {"class":""})
-                        movie_title.add(article[0].text)
+                    # Location - Where is it happening and etc.
+                    location = article[2].text
+                    movie_location.add(location)
 
-                        # Location - Where is it happening and etc.
-                        location = article[2].text
-                        movie_location.add(location)
+                    # Date - Gets the date of the movie
+                    date = li.find("span", {"class":"o-date"})
+                    #movie_date.add(date.text)
+                    movie_date.append(date.text)
 
-                        # Date - Gets the date of the movie
-                        date = li.find("span", {"class":"o-date"})
-                        movie_date.add(date.text)
+                    # Link - Use for scraping the links
+                    #button = li.find_all("button", {"class":"o-button o-button--green o-button--green-small"}).attrs['onclick']
 
-                        # Link - Use for scraping that new link
+                    # Theres a lot of buttons but we have to make sure that one of them contains a http.
+                    for bttn in li.select('button[onclick*="http"]'):
+                        # Now we fetch the data from 'onclick' in our bttn and it will retrieve the link.
+                        movie_link.add(bttn['onclick'].split("'")[1])
+                    IsError = False
 
-    except Exception:
-        print(Exception + " If the bug persist please issue a github report regarding the issue.")
+    except Exception as e:
+        IsError = True
+        if IsDebugging == True:
+            ErrorMessage.clear()
+            ErrorMessage.add("No Results | " + str(e))
+        elif IsDebugging == False:
+            ErrorMessage.add("No Results")
 
     # We're going to make a data where the results of our scraping are gonna be stored to.
     # The reason why we have to put 'list(object)' is to make it into a list inorder for it to append to our movie_result list.
     data = {
+        'results': str(len(movie_title)),
         'title': list(movie_title),
         'location': list(movie_location),
-        'date': list(movie_date)
+        'date': movie_date,
+        'link': list(movie_link)
     }
 
     # Now we give the data to the movie_result
     movie_result.append(data)
     print("[DrX] Finished Appending")
+    
+    # Debugging is needed when it comes to big projects, so we have to create one
+    #if IsDebugging == True:
+        #print("[DrX] Debugging: " + str(IsDebugging))
+        #if IsError == True:
+            #print("[DrX] Error: " + str(IsError))
+            #knownerrors = json.loads(open("errors.json").read())
+            #for error in knownerrors:
+                #if list(ErrorMessage) in knownerrors:
+                    #print(error[ErrorMessage])
+                
+
+
 
     # Returns the movie_result so that we can use StartScrape from any other python scripts as long as it is imported.
-    return movie_result
+    if IsError == False:
+        return movie_result
+    elif IsError == True:
+        return ErrorMessage
                         
     #for x in range(len(list(data))):
     #    movie_result.append(list(data)[x])
+
+def FetchSelectedResult(link):
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, features='lmxl')
+    print("[DrX] Scraping selected Result ./.")
 
 # =======================================================================================================#
 #                                                                                                        #
@@ -143,6 +190,7 @@ def StartScrape():
 
 # =======================================================================================================#
 # OLD METHOD EXAMPLES
+# WILL NOT WORK ANYMORE!
 #
 # Some Examples that worked with my previous method but now works with a new method that I came up with.
 #
@@ -159,19 +207,28 @@ def StartScrape():
 # NEW METHOD EXAMPLES
 
 # Gets only the "title" in a list
-#for article in StartScrape():
+#for article in FetchMovieResults():
 #    print(article['title'])
 
 # Gets only the "location" in a list
-#for article in StartScrape():
+#for article in FetchMovieResults():
 #    print(article['location'])
 
 # Gets only the "date" in a list
-#for article in StartScrape():
+#for article in FetchMovieResults():
 #    print(article['date'])
 
+# Gets the 'link' to the website to be scraped.
+#for article in FetchMovieResults():
+#    print(article['link'])
+
+# Specify what no of list to show.
+#for article in FetchMovieResults():
+#    print(article['link'][0])
+
 # Gets all the list of search results.
-for article in StartScrape():
+for article in FetchMovieResults():
+    #print(article['title'])
     print(article)
-#
+
 # =======================================================================================================#
