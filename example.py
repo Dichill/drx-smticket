3657f2baf6770d787da175d9893f2b7e061b3546
Dichill <48512644+Dichill@users.noreply.github.com>
2020-11-12 17:14:18 +0800

    Update example.py

--- example.py
+++ example.py
@@ -18,6 +18,10 @@ import scraper
 
 scraper.Search = input("What to search? ")
 
-for article in scraper.FetchMovieResults():
-    for x in range(0, int(article['results'])):
-        print(article['title'][x] + " | " + article['date'][x] + " | " + article['location'][x] + " | " + article['link'][x] + "\n")
+# Add a try and except in order to indicate what happend and why it gave us an error.
+try:
+    for article in scraper.FetchMovieResults():
+        for x in range(0, int(article['results'])):
+            print(article['title'][x] + " | " + article['date'][x] + " | " + article['location'][x] + " | " + article['link'][x] + "\n")
+except Exception as e:
+    print(e)
